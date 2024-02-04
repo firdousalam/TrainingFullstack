9faed701a020459b8a3687124f2f4d691bd756df
firdous alam <firdous.alam2058@gmail.com>
2024-02-04 16:28:57 +0530

    Code Update

---
+++ firdous/src/main/java/com/technophile/firdous/FirdousApplication.java
@@ -0,0 +1,13 @@
+package com.technophile.firdous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirdousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirdousApplication.class, args);
+	}
+
+}
---
+++ firdous/src/test/java/com/technophile/firdous/FirdousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technophile.firdous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirdousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
